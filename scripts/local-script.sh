1f3d29b9b0c68a0ba4d1c09f5551927c3b256dfe
lschule <lschule@gmail.com>
2015-03-09 21:47:40 +0100

    nada install crm114 as well

---
+++ scripts/local-script.sh
@@ -0,0 +1,13 @@
+ #!/usr/bin/env bash
+
+echo ">>> Installing crm114 with python"
+ 
+sudo apt-get install crm114
+
+
+sudo apt-get install build-essential
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+
+
+sudo apt-get install python-pip
+sudo pip install crm114
